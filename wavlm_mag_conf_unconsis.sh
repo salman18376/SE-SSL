7f64a4634ad1df716ce362b16667853eaf172f9f
Muhammad Salman Khan <50940931+salman18376@users.noreply.github.com>
2024-02-17 11:09:51 +0100

    Update wavlm_best.sh
    config with best scores

--- wavlm_mag_conf_unconsis.sh
+++ wavlm_mag_conf_unconsis.sh
@@ -2,8 +2,8 @@
 export CUDA_VISIBLE_DEVICES=3
 
 
-mkdir -p mag_only/wavlm_mag_conf_uncosis_consis
-mkdir -p reconstructed_audio/mag_only/wavlm_mag_conf_uncosis_consis
+mkdir -p mag_only/wavlm_best
+mkdir -p reconstructed_audio/mag_only/wavlm_best
 mkdir -p results/mag_only
 
 python train_disc.py \
@@ -12,20 +12,20 @@ python train_disc.py \
     --batch_size 4\
     --cuda  \
     --model_tag microsoft/wavlm-large \
-    --checkpoint_dir mag_only/wavlm_mag_conf_uncosis_consis/ \
-    --reconstructed_audio_folder reconstructed_audio/mag_only/wavlm_mag_conf_uncosis_consis \
+    --checkpoint_dir mag_only/wavlm_best/ \
+    --reconstructed_audio_folder reconstructed_audio/mag_only/wavlm_best \
     --compute_metrics_interval 1\
     --magnitude_head conformer\
-    --experiment_name mag_only/metric_wavlm_mag_conf_uncosis_consis \
+    --experiment_name mag_only/wavlm_best \
     --log_on_comet 
 
 python compute_metrics_v3.py \
     --experiment_config configs/mpnet_weights.json \
-    --model_checkpoint /home/salman/SE_Self-Supervise_Learning-/mag_only/best_model.pt \
+    --model_checkpoint /home/salman/SE_Self-Supervise_Learning-/mag_only/wavlm_best/best_model.pt \
     --cuda \
-    --reconstructed_audio_folder reconstructed_audio/mag_only/wavlm_mag_conf_uncosis_consis \
+    --reconstructed_audio_folder reconstructed_audio/mag_only/wavlm_best \
     --model_tag microsoft/wavlm-large \
-    --magnitude_head conformer > results/mag_only/wavlm_mag_conf_uncosis_consis.txt
+    --magnitude_head conformer > results/mag_only/wavlm_best.txt
 
 
 
